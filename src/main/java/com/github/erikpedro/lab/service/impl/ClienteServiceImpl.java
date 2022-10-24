9794bec6d134b16eac04f8a985d3afbbd2f5f9cf
erikpedro <erikpedro@gmail.com>
2022-10-23 22:38:36 -0300

    Padroes de projeto em uma API REST

--- src/main/java/com/github/erikpedro/lab/service/impl/ClienteServiceImpl.java
+++ src/main/java/com/github/erikpedro/lab/service/impl/ClienteServiceImpl.java
@@ -1,33 +1,69 @@
 package com.github.erikpedro.lab.service.impl;
 
+import com.github.erikpedro.lab.model.Address;
 import com.github.erikpedro.lab.model.Cliente;
+import com.github.erikpedro.lab.repository.AddressRepository;
+import com.github.erikpedro.lab.repository.ClienteReopository;
 import com.github.erikpedro.lab.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+/**
+ * The type Cliente service.
+ */
 @Service
 public class ClienteServiceImpl implements ClienteService {
+
+    @Autowired
+    private ClienteReopository clienteReopository;
+
+    @Autowired
+    private AddressRepository addressRepository;
+
+    @Autowired
+    private ViaCepService viaCepService;
+
     @Override
     public Iterable<Cliente> searchAll() {
-        return null;
+        return clienteReopository.findAll();
     }
 
     @Override
     public Cliente findbyId(Long id) {
-        return null;
+        Optional<Cliente> cliente = clienteReopository.findById(id);
+        return cliente.get();
     }
 
     @Override
     public void insert(Cliente cliente) {
-
+        persist(cliente);
     }
+
+
 
     @Override
     public void update(Long id, Cliente cliente) {
+        Optional<Cliente> clienteDb = clienteReopository.findById(id);
+        if(clienteDb.isPresent()){
+            persist(cliente);
+        }
 
     }
 
     @Override
     public void delete(Long id) {
+        clienteReopository.deleteById(id);
+    }
 
+    private void persist(Cliente cliente) {
+        String cep = cliente.getAddress().getCep();
+        Optional<Address> address = addressRepository.findById(cep);
+        address.orElseGet(() -> {
+            Address Newaddress = viaCepService.searchCep(cep);
+            addressRepository.save(Newaddress);
+            return Newaddress;
+        });
     }
 }
